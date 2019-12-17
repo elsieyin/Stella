973f8aa83b59cc9648f184d07fa60243fd208098
Elsie Yin <tizyin@gmail.com>
2019-12-17 20:12:59 +0800

    Maybe i copyed the wrong file
    I don't know seem quite alike to me
    just some basic language model

--- SequenceToSequence.py
+++ SequenceToSequence.py
@@ -4,146 +4,86 @@ import csv
 import random
 import numpy as np
 
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+import tensorflow.keras.utils as ku
+import numpy as np
+
 # tf 2.x python3.x
 # load training data
 
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras import regularizers
+data = open('/data/trainSet').read()
+
+corpus = data.lower().split("\n")
+# Some data preprocessing probably on another file maybe just import module
+
+tokenizer.fit_on_texts(corpus)
+# Not sure if total_words necessary
+total_words = len(tokenizer.word_index) + 1
 
 
-embedding_dim = 100
-max_length = 16
-trunc_type='post'
-padding_type='post'
-oov_tok = "<OOV>"
-training_size=160000
-test_portion=.1
-
-corpus = []
-
-num_sentences = 0
-
-with open("/tmp/training_cleaned.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        list_item=[]
-        list_item.append(row[5])
-        this_label=row[0]
-        if this_label=='0':
-            list_item.append(0)
-        else:
-            list_item.append(1)
-        num_sentences = num_sentences + 1
-        corpus.append(list_item)
-
-print(num_sentences)
-print(len(corpus))
-print(corpus[1])
-
-sentences=[]
-labels=[]
-random.shuffle(corpus)
-for x in range(training_size):
-    sentences.append(corpus[x][0])
-    labels.append(corpus[x][1])
+# create input sequences using list of tokens
+input_sequences = []
+for line in corpus:
+	token_list = tokenizer.texts_to_sequences([line])[0]
+	for i in range(1, len(token_list)):
+		n_gram_sequence = token_list[:i+1]
+		input_sequences.append(n_gram_sequence)
 
 
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(sentences)
+# pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
 
-word_index = tokenizer.word_index
-vocab_size=len(word_index)
+# create predictors and label
+predictors, label = input_sequences[:,:-1],input_sequences[:,-1]
 
-sequences = tokenizer.texts_to_sequences(sentences)
-padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+label = ku.to_categorical(label, num_classes=total_words)
 
-split = int(test_portion * training_size)
+model = Sequential()
+model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
+model.add(Bidirectional(LSTM(150, return_sequences = True)))
+model.add(Dropout(0.2))
+model.add(LSTM(100))
+model.add(Dense(total_words/2, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(total_words, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
 
-test_sequences = padded[0:split]
-training_sequences = padded[split:training_size]
-test_labels = labels[0:split]
-training_labels = labels[split:training_size]
+history = model.fit(predictors, label, epochs=100, verbose=1)
 
-# Note this is the 100 dimension version of GloVe from Stanford
-# I unzipped and hosted it on my site to make this notebook easier
-!wget --no-check-certificate \
-    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/glove.6B.100d.txt \
-    -O /tmp/glove.6B.100d.txt
-
-embeddings_index = {};
-with open('/tmp/glove.6B.100d.txt') as f:
-    for line in f:
-        values = line.split();
-        word = values[0];
-        coefs = np.asarray(values[1:], dtype='float32');
-        embeddings_index[word] = coefs;
-
-embeddings_matrix = np.zeros((vocab_size+1, embedding_dim));
-for word, i in word_index.items():
-    embedding_vector = embeddings_index.get(word);
-    if embedding_vector is not None:
-        embeddings_matrix[i] = embedding_vector;
-
-print(len(embeddings_matrix))
-# Expected Output
-# 138859
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size+1, embedding_dim, input_length=max_length, weights=[embeddings_matrix], trainable=False),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv1D(64, 5, activation='relu'),
-    tf.keras.layers.MaxPooling1D(pool_size=4),
-    tf.keras.layers.LSTM(64),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.summary()
-
-num_epochs = 50
-history = model.fit(training_sequences, training_labels, epochs=num_epochs, validation_data=(test_sequences, test_labels), verbose=2)
-
-print("Training Complete")
-
-import matplotlib.image  as mpimg
 import matplotlib.pyplot as plt
+acc = history.history['acc']
+loss = history.history['loss']
 
-#-----------------------------------------------------------
-# Retrieve a list of list results on training and test data
-# sets for each training epoch
-#-----------------------------------------------------------
-acc=history.history['acc']
-val_acc=history.history['val_acc']
-loss=history.history['loss']
-val_loss=history.history['val_loss']
+epochs = range(len(acc))
 
-epochs=range(len(acc)) # Get number of epochs
-
-#------------------------------------------------
-# Plot training and validation accuracy per epoch
-#------------------------------------------------
-plt.plot(epochs, acc, 'r')
-plt.plot(epochs, val_acc, 'b')
-plt.title('Training and validation accuracy')
-plt.xlabel("Epochs")
-plt.ylabel("Accuracy")
-plt.legend(["Accuracy", "Validation Accuracy"])
+plt.plot(epochs, acc, 'b', label='Training accuracy')
+plt.title('Training accuracy')
 
 plt.figure()
 
-#------------------------------------------------
-# Plot training and validation loss per epoch
-#------------------------------------------------
-plt.plot(epochs, loss, 'r')
-plt.plot(epochs, val_loss, 'b')
-plt.title('Training and validation loss')
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.legend(["Loss", "Validation Loss"])
+plt.plot(epochs, loss, 'b', label='Training Loss')
+plt.title('Training loss')
+plt.legend()
 
-plt.figure()
+plt.show()
 
+seed_text = "Help me Obi Wan Kenobi, you're my only hope"
+next_words = 100
 
-# Expected Output
-# A chart where the validation loss does not increase sharply!
+for _ in range(next_words):
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+    predicted = model.predict_classes(token_list, verbose=0)
+    output_word = ""
+    for word, index in tokenizer.word_index.items():
+        if index == predicted:
+            output_word = word
+            break
+    seed_text += " " + output_word
+print(seed_text)
